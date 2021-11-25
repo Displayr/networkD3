6fada7e855568d30f779472f1000f4e54b8add7b
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-11-26 09:52:23 +1100

    Fix test

--- inst/htmlwidgets/lib/sankeyNetworkDefinition.test.js
+++ inst/htmlwidgets/lib/sankeyNetworkDefinition.test.js
@@ -2,9 +2,6 @@ const sankeyNetworkDefinition = require('./sankeyNetworkDefinition');
 
 const puppeteer = require('puppeteer');
 
-const { toMatchImageSnapshot } = require('jest-image-snapshot');
-expect.extend({ toMatchImageSnapshot });
-
 var data = {
     "links": {
         "source": [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10],
